bc8115ee75fb03bac8251cccb2f35d2edb542edd
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-31 17:11:39 -0300

    mobile buttons component

--- src/components/base/Menu/Mobile/MobileButton/index.tsx
+++ src/components/base/Menu/Mobile/MobileButton/index.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
-export interface ButtonProps {
+interface ButtonProps {
   id?: number;
   buttonText: string;
   Icon: React.ElementType;
   path: string;
+  setNav: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const MenuButton = ({ buttonText, Icon, path }: ButtonProps) => {
+const MobileButton = ({ buttonText, Icon, path, setNav }: ButtonProps) => {
   return (
-    <div className="w-full h-12 ">
-      <Link href={path}>
-        <div className="bg-[#1F2B3D] w-full h-full px-5 flex flex-row items-center rounded-sm hover:bg-base-400 transition-colors">
-          <Icon className="mr-2 h-4 w-4" />
-          {buttonText}
-        </div>{" "}
-      </Link>
-    </div>
+    <Link
+    onClick={() => setNav(false)}
+    href={path}
+    className="w-[70vw] h-[6vh] lg:w-[40vw] xl:w-[30vw] 2xl:w-[20vw] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p4 cursor-pointer hover:scale-110 ease-in duration-200"
+  >
+    <Icon size={20} className="text-black" />
+    <span className="pl-4 text-black">{buttonText}</span>
+  </Link>
   );
 };
-export default MenuButton;
+export default MobileButton;
--- src/components/base/Menu/Mobile/index.tsx
+++ src/components/base/Menu/Mobile/index.tsx
@@ -2,6 +2,8 @@
 
 import { Mail, Delete } from "lucide-react";
 import { useState } from "react";
+import { buttons } from "../data";
+import MobileButton from "./MobileButton";
 
 const Mobile = () => {
   const [nav, setNav] = useState(false);
@@ -24,46 +26,15 @@ const Mobile = () => {
       )}
       {nav ? (
         <div className="fixed w-full h-screen bg-white/40 flex flex-col justify-center items-center z-[20] gap-6">
-          <a
-            onClick={handleNavState}
-            href="#main"
-            className="w-[70vw] h-[6vh] lg:w-[40vw] xl:w-[30vw] 2xl:w-[20vw] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <Mail size={20} />
-            <span className="pl-4"></span>
-          </a>
-          <a
-            onClick={handleNavState}
-            href="#work"
-            className="w-[70vw] h-[6vh] lg:w-[40vw] xl:w-[30vw] 2xl:w-[20vw] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <Mail size={18} />
-            <span className="pl-4"></span>
-          </a>
-          <a
-            onClick={handleNavState}
-            href="#projects"
-            className="w-[70vw] h-[6vh] lg:w-[40vw] xl:w-[30vw] 2xl:w-[20vw] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <Mail size={20} />
-            <span className="pl-4"></span>
-          </a>
-          <a
-            onClick={handleNavState}
-            href="#main"
-            className="w-[70vw] h-[6vh] lg:w-[40vw] xl:w-[30vw] 2xl:w-[20vw] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <Mail size={20} />
-            <span className="pl-4"></span>
-          </a>
-          <a
-            onClick={handleNavState}
-            href="#contact"
-            className="w-[70vw] h-[6vh] lg:w-[40vw] xl:w-[30vw] 2xl:w-[20vw] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <Mail size={20} />
-            <span className="pl-4"></span>
-          </a>
+         {buttons.map((button) => (
+            <MobileButton
+              key={button.id}
+              buttonText={button.buttonText}
+              Icon={button.Icon}
+              path={button.path}
+              setNav={setNav}
+            />
+          ))}
         </div>
       ) : (
         ""
