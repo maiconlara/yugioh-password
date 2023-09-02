6c04d18f5490715704e60b17cd832c6604ee85fa
Maicon <maiconluan.lara@yahoo.com.br>
2023-09-02 00:22:10 -0300

    Menu selected animation + logo

--- src/components/base/Menu/Desktop/DesktopButton/index.tsx
+++ src/components/base/Menu/Desktop/DesktopButton/index.tsx
@@ -4,17 +4,34 @@ export interface ButtonProps {
   buttonText: string;
   Icon: React.ElementType;
   path: string;
+  setSelectedPath: React.Dispatch<React.SetStateAction<string>>;
+  selectedPath: string;
 }
 
-const DesktopButton = ({ buttonText, Icon, path }: ButtonProps) => {
+const DesktopButton = ({
+  buttonText,
+  Icon,
+  path,
+  setSelectedPath,
+  selectedPath
+}: ButtonProps) => {
   return (
     <div className="w-full h-12 ">
-      <Link href={path}>
-        <div className="bg-[#000] w-full h-full px-5 flex flex-row items-center rounded-sm hover:border-gray-300/40 hover:border-[1px] transition-transform hover:translate-x-2">
-          <Icon className="mr-2 h-4 w-4" />
-          {buttonText}
-        </div>{" "}
-      </Link>
+      {selectedPath === buttonText ? (
+        <Link href={path} onClick={() => setSelectedPath(buttonText)}>
+          <div className="bg-[#000] w-full h-full px-5 flex flex-row items-center rounded-sm border-gray-300/40 border-[1px] transition-transform translate-x-2">
+            <Icon className="mr-2 h-4 w-4" />
+            {buttonText}
+          </div>{" "}
+        </Link>
+      ) : (
+        <Link href={path} onClick={() => setSelectedPath(buttonText)}>
+          <div className="bg-[#000] w-full h-full px-5 flex flex-row items-center rounded-sm hover:border-gray-300/40 hover:border-[1px] transition-transform hover:translate-x-2">
+            <Icon className="mr-2 h-4 w-4" />
+            {buttonText}
+          </div>{" "}
+        </Link>
+      )}
     </div>
   );
 };
--- src/components/base/Menu/Desktop/index.tsx
+++ src/components/base/Menu/Desktop/index.tsx
@@ -2,12 +2,17 @@
 
 import DesktopButton from "./DesktopButton";
 import { buttons } from "../data";
+import logo from "../../../../assets/logo.webp";
+import Image from "next/image";
+import { useState } from "react";
 
 const Desktop = () => {
+  const [selectedPath, setSelectedPath] = useState<string>("Home");
+
   return (
     <div className="hidden md:flex flex-col  w-full  py-5 justify-between items-start h-screen bg-black border-r-[1px] border-gray-300/40">
       <div className="flex flex-col w-full items-center  gap-6">
-        <h1>Yu-Gi-Oh!</h1>
+        <Image src={logo} alt="" width={200} height={200} />
         <div className="w-full bg-gray-300/40 h-px"></div>
       </div>
       <div className="flex flex-col items-start justify-between h-[280px] w-full -mt-16">
@@ -17,13 +22,16 @@ const Desktop = () => {
             buttonText={button.buttonText}
             Icon={button.Icon}
             path={button.path}
+            setSelectedPath={setSelectedPath}
+            selectedPath={selectedPath}
           />
         ))}
       </div>
       <div className="flex flex-col items-center justify-between h-20 w-full">
         <div className="w-full px-2 bg-gray-300/40 h-px"></div>
-        <div className="flex flex-1 items-center justify-center">
-          <h1>Meu Footer maneiro!</h1>
+        <div className="flex flex-col items-center justify-center">
+          <h1>Bem vindo à Yu-Gi-Oh! Password</h1>
+          <p className="text-xs pt-4">Feito Com 💙 por Maicon Lara</p>
         </div>
       </div>
     </div>
