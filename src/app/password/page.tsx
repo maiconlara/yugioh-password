6c6db4f33c5739b305b6efe4a774ff07740e1665
maiconlara <maiconluan.lara@yahoo.com.br>
2023-09-12 09:32:18 -0300

    adding isLoading and Placeholder card

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -1,24 +1,56 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { getCards} from "./../api/api";
+import { getCards } from "./../api/api";
 import { useState } from "react";
+import backCard from "@/assets/backCard.png";
+import { Loader2 } from "lucide-react";
 
 const Password = () => {
   const [cards, setCards] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleCardsApi = async () => {
+    setIsLoading(true);
     const cards = await getCards();
     setCards(cards);
+    setIsLoading(false);
   };
-
 
   return (
     <div className=" flex justify-center items-center h-screen bg-[#111111]">
       <div className="flex flex-col items-center gap-4">
-
-      <Image src={cards} width={328} height={484}  alt="" />
-      <Button onClick={handleCardsApi} className="font-bold uppercase hover:bg-transparent hover:text-blue-800 transition-colors">Buscar carta</Button>
+        {isLoading ? (
+          <div className="w-[246px] h-[363px] flex items-center justify-center"> 
+          <Loader2 className="text-white animate-spin w-12 h-12 " />
+          </div>
+        ) : (
+          <>
+            {cards ? (
+              <Image
+                src={cards}
+                width={246}
+                height={363}
+                alt=""
+                className="rounded-sm ease-in-out"
+              />
+            ) : (
+              <Image
+                src={backCard}
+                width={246}
+                height={363}
+                alt=""
+                className="rounded-sm ease-in-out"
+              />
+            )}
+          </>
+        )}
+        <Button
+          onClick={handleCardsApi}
+          className="font-bold uppercase rounded-sm bg-[#000] border-gray-300/40 border-[1px] hover:bg-transparent transition-colors"
+        >
+          Buscar carta
+        </Button>
       </div>
     </div>
   );
