2ead4c88704eabf73e4e07dfc759d59c74f81af8
maiconlara <maiconluan.lara@yahoo.com.br>
2023-09-12 16:49:37 -0300

    avatar component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,19 @@ import AboutCards from "@/components/base/AboutCards";
 const About = () => {
   return (
     <div className=" flex justify-center items-center h-screen bg-[#111111]">
-      <div className="flex flex-col items-center justify-center gap-6">
-        <AboutCards title="Portfolio" text="Take a look at my portfolio"/>
-        <AboutCards title="Github" text="Go checkout my GitHub"/>
+      <div className="flex flex-row items-center justify-center gap-6">
+        <AboutCards
+          title="Portfolio"
+          text="Take a look at my portfolio"
+          link="https://portfolio-maiconlara.vercel.app/"
+          image="https://github.com/maiconlara.png"
+        />
+        <AboutCards
+          title="Github"
+          text="Go checkout my GitHub"
+          link="https://github.com/maiconlara"
+          image="https://github.com/maiconlara.png"
+        />
       </div>
     </div>
   );
--- src/components/base/AboutCards/index.tsx
+++ src/components/base/AboutCards/index.tsx
@@ -1,25 +1,33 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ChevronRight } from "lucide-react";
 
 interface AboutCardProps {
-    title: string;
-    text: string;
+  title: string;
+  text: string;
+  link: string;
+  image: string;
 }
 
-
-
-
-const AboutCards = ({title, text} : AboutCardProps) => {
+const AboutCards = ({ title, text, image, link }: AboutCardProps) => {
   return (
-    <div className="h-[120px] w-[280px] rounded-md border bg-black border-gray-300/40 justify-start space-x-4 py-4 px-4 flex flex-row  hover:scale-105 transition-transform duration-300">
-      <div>
+    <div className="h-[120px] w-[280px] rounded-md border bg-black border-gray-300/40 justify-start space-x-4 py-4 px-4 flex flex-col">
+      <div className="flex flex-row w-full">
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
+          <AvatarImage src={image} />
+          <AvatarFallback>hi</AvatarFallback>
         </Avatar>
+        <div className="space-y-1 ml-4 w-full">
+          <p className="text-sm font-bold">{title}</p>
+          <p className="text-sm font-regular">{text}</p>
+        </div>
       </div>
-      <div className="space-y-1">
-        <p className="text-sm font-bold">{title}</p>
-        <p className="text-sm font-regular">{text}</p>
+      <div className="self-end text-sm py-6 font-bold uppercase flex flex-row items-center">
+        <a
+          href={link}
+          className="flex items-center hover:text-gray-100 transition-transform hover:translate-x-1"
+        >
+          <ChevronRight className="h-4 w-4 text-white" /> Visitar
+        </a>
       </div>
     </div>
   );
