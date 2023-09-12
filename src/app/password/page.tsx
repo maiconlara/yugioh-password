8191047f56179ad31f3097fa2f719acc2414a694
maiconlara <maiconluan.lara@yahoo.com.br>
2023-09-12 10:35:44 -0300

    adding input to password page

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -5,6 +5,7 @@ import { getCards } from "./../api/api";
 import { useState } from "react";
 import backCard from "@/assets/backCard.png";
 import { Loader2 } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 const Password = () => {
   const [cards, setCards] = useState("");
@@ -21,8 +22,8 @@ const Password = () => {
     <div className=" flex justify-center items-center h-screen bg-[#111111]">
       <div className="flex flex-col items-center gap-4">
         {isLoading ? (
-          <div className="w-[246px] h-[363px] flex items-center justify-center"> 
-          <Loader2 className="text-white animate-spin w-12 h-12 " />
+          <div className="w-[246px] h-[363px] flex items-center justify-center">
+            <Loader2 className="text-white animate-spin w-12 h-12 " />
           </div>
         ) : (
           <>
@@ -45,12 +46,20 @@ const Password = () => {
             )}
           </>
         )}
-        <Button
-          onClick={handleCardsApi}
-          className="font-bold uppercase rounded-sm bg-[#000] border-gray-300/40 border-[1px] hover:bg-transparent transition-colors"
-        >
-          Buscar carta
-        </Button>
+        <div className="flex flex-row items-center justify-center gap-2 pt-4 w-[400px]">
+          <Input
+            type="number"
+            placeholder="ex. 68752475"
+            className="bg-transparent border-[1px] border-gray-300/40 focus:ring-gray-500 focus:ring-1 focus:outline-none ring-offset-0 content-center"
+            maxLength={2}
+          />
+          <Button
+            onClick={handleCardsApi}
+            className="font-bold uppercase rounded-sm bg-[#000] border-gray-300/40 border-[1px] hover:bg-transparent transition-colors w-auto min-w-fit"
+          >
+            Buscar carta
+          </Button>
+        </div>
       </div>
     </div>
   );
