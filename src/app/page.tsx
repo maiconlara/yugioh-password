40b2ec3991eaffb4e04a5d70c4d14861cdbf89ca
maiconlara <maiconluan.lara@yahoo.com.br>
2023-09-25 09:29:27 -0300

    home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-
-
 export default function Home() {
-
-
   return (
-    <div className=" flex justify-center   items-center h-screen bg-[#111111]">
-      <p className="text-white text-center max-w-[400px] font-regular">Yu-Gi-Oh! App é uma aplicação feita para fins de treino Front-end divirtá-se</p>
-
+    <div className=" flex justify-center flex-col  items-center h-screen bg-[#111111] gap-8">
+      <h1 className="text-white md:text-4xl text-3xl md:max-w-[500px] max-w-[400px] font-bold">
+        Bem-vindo ao Yu-Gi-Oh! App
+      </h1>
+      <p className="text-white text-center max-w-[400px] font-regular ">
+        Aplicação feita para pesquisar cartas, ver informações e testar os
+        passwords.
+      </p>
     </div>
   );
 }
