449f6e0c0bd12eb7ebef8e250c596a2254a6cbd2
maiconlara <maiconluan.lara@yahoo.com.br>
2024-02-01 15:04:45 -0300

    refactor: changing justify and gap in about cards

--- src/components/base/AboutCards/index.tsx
+++ src/components/base/AboutCards/index.tsx
@@ -11,7 +11,7 @@ interface AboutCardProps {
 
 const AboutCards = ({ title, text, image, link, fallback }: AboutCardProps) => {
   return (
-    <div className="h-[120px] w-[280px] rounded-md border bg-black border-gray-300/40 justify-start space-x-4 py-4 px-4 flex flex-col">
+    <div className="h-[140px] w-[280px] rounded-md border bg-black border-gray-300/40 justify-between gap-3 py-4 px-4 flex flex-col">
       <div className="flex flex-row w-full">
         <Avatar>
           <AvatarImage  src={image} />
@@ -22,7 +22,7 @@ const AboutCards = ({ title, text, image, link, fallback }: AboutCardProps) => {
           <p className="text-sm font-regular">{text}</p>
         </div>
       </div>
-      <div className="self-end text-sm py-6 font-bold uppercase flex flex-row items-center">
+      <div className="self-end text-sm font-bold uppercase flex flex-row items-center">
         <a
           href={link}
           className="flex items-center hover:text-gray-100 transition-transform hover:translate-x-1"
